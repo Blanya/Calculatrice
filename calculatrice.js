298f2933a83fb6c3ad21e33aee2db7886724de7f
Beudaert Marianne <x_Mariannee@hotmail.fr>
2019-07-25 16:19:48 +0200

    First JS, affichage value

---
+++ calculatrice.js
@@ -0,0 +1,15 @@
+function affichage()
+{  
+   
+     document.getElementsByClassName("answer").value;
+    
+     }
+
+
+
+
+let buttons = document.getElementsByTagName("button");
+for(let i = 0; i < buttons.length ; i ++ ) 
+{ 
+buttons[i].addEventListener("click", affichage);
+} 
