b13f233e6837424b68a77974b01f4f564999ba06
Beudaert Marianne <x_Mariannee@hotmail.fr>
2019-07-25 17:13:42 +0200

    Modification JS, second version, add clear

--- calculatrice.js
+++ calculatrice.js
@@ -1,15 +1,18 @@
 function affichage()
 {  
-   
-     document.getElementsByClassName("answer").value;
-    
-     }
-
+     //console.log(this.value);
+     let answer = document.getElementById("answer");
+     answer.textContent=this.value;
+}
 
 
 
 let buttons = document.getElementsByTagName("button");
 for(let i = 0; i < buttons.length ; i ++ ) 
-{ 
+{
 buttons[i].addEventListener("click", affichage);
 } 
+
+let clear = document.getElementById("reset");
+clear.addEventListener("click", reset);
+
